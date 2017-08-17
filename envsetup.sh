d042311d010f99902ea687c3902aa272b198cdef
Luca Stefani <luca020400@lineageos.org>
2017-08-17 21:21:23 +0200

    build: Add Six build support
    Squashed also with the following changes:
    
    Author: Luca Stefani <luca020400@lineageos.org>
    Date:   Thu Aug 17 22:22:13 2017 +0200
    
    core: Include Six pathmap
    
    Change-Id: Ie25dee383cc5bc9bb6390cff2cb2460d526d80b6
    
    Author: Simon Shields <simon@lineageos.org>
    Date:   Fri Mar 2 12:55:47 2018 +1100
    
    build: unconditionally use Six pathmap
    
    even if we're not building a Six target, we need to
    use the Six pathmap to make things like recovery and ril
    happy
    
    Change-Id: I974c30ad10d4ff5b7805c3df9c22010f1e002bf1
    
    Change-Id: Iab8b2ef8b644a7bea10bfd28b99d9e69539b1357

--- envsetup.sh
+++ envsetup.sh
@@ -137,6 +137,14 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+    if (echo -n $1 | grep -q -e "^sixrom_") ; then
+        SIX_BUILD=$(echo -n $1 | sed -e 's/^sixrom_//g')
+        export BUILD_NUMBER=$( (date +%s%N ; echo $SIX_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10 )
+    else
+        SIX_BUILD=
+    fi
+    export SIX_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
@@ -622,6 +630,8 @@ function lunch()
         echo "Invalid lunch combo: $selection"
         return 1
     fi
+
+    check_product $product
 
     TARGET_PRODUCT=$product \
     TARGET_BUILD_VARIANT=$variant \
